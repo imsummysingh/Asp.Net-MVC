1ee22058cb7fb861e77778bac8db85fd1aa82869
Summy Singh <summysingh86@gmail.com>
2020-12-15 19:30:52 +0530

    added

---
+++ MVCDemo/MVCDemo/Models/EmployeeContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        //public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
