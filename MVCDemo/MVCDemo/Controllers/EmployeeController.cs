d41cae9ebfab8b2f4f5cd7a5488c4f2334fa546c
Summy Singh <summysingh86@gmail.com>
2020-12-17 16:58:31 +0530

    added

--- MVCDemo/MVCDemo/Controllers/EmployeeController.cs
+++ MVCDemo/MVCDemo/Controllers/EmployeeController.cs
@@ -1,45 +1,26 @@
-﻿using MVCDemo.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BusinessLayer;
 
 namespace MVCDemo.Controllers
 {
     public class EmployeeController : Controller
     {
-        //public ActionResult Details()
-        //{
-        //    Employee employee = new Employee()
-        //    {
-        //        EmployeeId = 101,
-        //        Name = "John",
-        //        Gender = "Male",
-        //        City = "London"
-        //    };
-        //    return View(employee);
-        //}
-        //public ActionResult Index()
-        //{
-        //    EmployeeContext employeeContext = new EmployeeContext();
-        //    List<Employee> employees = employeeContext.Employees.ToList();
-
-        //    return View(employees);
-        //}
-        public ActionResult Index(int departmentId)
+        // GET: Employee
+        public ActionResult Index()
         {
-            EmployeeContext employeeContext = new EmployeeContext();
-            List<Employee> employees = employeeContext.Employees.Where(emp => emp.DepartmentId == departmentId).ToList();
-
-            return View(employees);
+           EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+           List<Employee> employees = employeeBusinessLayer.Employees.ToList();
+           return View(employees);
         }
-        public ActionResult Details(int id)
-        {
-            EmployeeContext employeeContext = new EmployeeContext();
-            Employee employee = employeeContext.Employees.Single(x => x.EmployeeId == id);
 
-            return View(employee);
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
         }
     }
 }
---
+++ MVCDemo/MVCDemo/Controllers/EmployeeController1.cs
@@ -0,0 +1,45 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class EmployeeController1 : Controller
+    {
+        //public ActionResult Details()
+        //{
+        //    Employee employee = new Employee()
+        //    {
+        //        EmployeeId = 101,
+        //        Name = "John",
+        //        Gender = "Male",
+        //        City = "London"
+        //    };
+        //    return View(employee);
+        //}
+        //public ActionResult Index()
+        //{
+        //    EmployeeContext employeeContext = new EmployeeContext();
+        //    List<Employee> employees = employeeContext.Employees.ToList();
+
+        //    return View(employees);
+        //}
+        public ActionResult Index(int departmentId)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            List<Employee> employees = employeeContext.Employees.Where(emp => emp.DepartmentId == departmentId).ToList();
+
+            return View(employees);
+        }
+        public ActionResult Details(int id)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee employee = employeeContext.Employees.Single(x => x.EmployeeId == id);
+
+            return View(employee);
+        }
+    }
+}
